9175ba3e7b4e180458520aba6e77f8423e0b570b
daycorvalan09 <62397143+daycorvalan09@users.noreply.github.com>
2020-05-25 11:22:36 -0300

    Update index.js

--- projeto/src/Pages/Product/index.js
+++ projeto/src/Pages/Product/index.js
@@ -24,7 +24,7 @@ export default class Product extends Component {
                 <h1>{product.title}</h1>
                 <p>{product.description}</p>
                 <p>
-                    URL: <a href ={product.url}>{product.url}</a>
+                    URL: <Link href ={product.url}>{product.url}</Link>
                 </p>
             </div>
         );
